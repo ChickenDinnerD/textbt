5bf2db56ee6d46c1aadc6ba6a267e5fee2dc4d97
ChickenDinnerD <alexey333m@gmail.com>
2023-03-03 16:50:55 +0200

    frontend work

--- client/src/App.js
+++ client/src/App.js
@@ -1,84 +1,68 @@
-import { React, useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
-import { createRoot } from 'react-dom/client';
+import "./App.css";
 
 export default function App() {
-
-  const [notes, setNotes] = useState(null)
+  const [notes, setNotes] = useState(null);
+  const [showButton, setShowButton] = useState(true);
 
   const fetchData = async () => {
+    const noteList = await axios.get("http://localhost:8080/service");
+    setNotes(noteList.data[0]);
+    console.log(noteList);
+    setShowButton(false);
+  };
 
-    const noteList = await axios.get(
-        "http://localhost:8080/service"
-      )
-      setNotes(noteList.data[0]);
-    
-      console.log(noteList);
-  }
-
-  fetchData();
-
-
-  const notesList = document.getElementById("notes-list");
-
-  function renderNotes() {
-    notes.forEach(note => {
-      const listItem = document.createElement("li");
-      listItem.textContent = note.noteName;
-      listItem.addEventListener("click", () => {
-        const modal = document.createElement("div");
-        modal.classList.add("modal");
-        const modalContent = document.createElement("div");
-        modalContent.classList.add("modal-content");
-        modalContent.textContent = note.noteText;
-        modal.appendChild(modalContent);
-        document.body.appendChild(modal);
-      });
-      notesList.appendChild(listItem);
-    });
-  }
-
-  const openNotesButton = document.getElementById("open-notes-button");
-openNotesButton.addEventListener("click", () => {
-  renderNotes();
-  openNotesButton.style.display = "none";
-});
-  openNotesButton.addEventListener("click", renderNotes);
-
-
-  function AppWithCallbackAfterRender() {
-    useEffect(() => {
-      console.log('rendered');
-    });
-  
-    return <App tab="home" />
-  }
-
-AppWithCallbackAfterRender();
+  const closeModal = () => {
+    const modal = document.querySelector(".modal");
+    document.body.removeChild(modal);
+  };
 
   return (
-  <div id="root" className="App">
+    <div id="root" className="App">
+      <h1>БТ/</h1>
 
-          <h1>БТ/</h1>
-
-          <h2>
-          бессмысленный текст, безымянный, безумный текст, бесполезный текст, бессловесный,
-            багровый текст, бывалый, бездомный текст, безосудительный текст, больной, белый, балет, 
-            берлога, Бостон, биполярный, быдло
-          </h2>
-
+      <h2>
+        бессмысленный текст, безымянный, безумный текст, бесполезный текст,
+        бессловесный, багровый текст, бывалый, бездомный текст, безосудительный
+        текст, больной, белый, балет, берлога, Бостон, биполярный, быдло
+      </h2>
 
       <div>
+        {showButton && (
+          <button id="open-notes-button" onClick={fetchData}>
+            Открыть заметки
+          </button>
+        )}
 
-      <button id="open-notes-button">Открыть заметки</button>
-
+        {notes && (
+          <ul id="notes-list">
+            {notes.map((note) => (
+              <li
+                key={note.id}
+                onClick={() => {
+                  const modal = document.createElement("div");
+                  modal.classList.add("modal");
+                  const modalContent = document.createElement("div");
+                  modalContent.classList.add("modal-content");
+                  modalContent.textContent = note.noteText;
+                  const closeButton = document.createElement("button");
+                  closeButton.textContent = "свернуть(не туда)";
+                  closeButton.addEventListener("click", closeModal);
+                  modalContent.appendChild(closeButton);
+                  modal.appendChild(modalContent);
+                  document.body.appendChild(modal);
+                }}
+              >
+                {note.noteName}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-
-  </div>
-);
+    </div>
+  );
 }
 
-const container = document.getElementById("root")
-const root = createRoot(container);
-root.render(<App tab="home" />);
+ReactDOM.render(<App />, document.getElementById("root"));
