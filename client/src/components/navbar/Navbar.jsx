f58ed5488439452452f4aff1afb0efc8c62f8612
immelnyk <tarasmelnyk3@gmail.com>
2023-04-28 23:13:34 +0300

    fix

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-center py-4 bg-gray-50 border border-b fixed top-0 w-full">
       <div>
-        <div onClick={() => window.location.reload()} className="nav-row">
+        <div onClick={() => window.location.reload()}>
           <strong>БТ/</strong> безімянний
         </div>
       </div>
