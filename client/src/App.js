d85913b9db31a155c6ed345e4e0717a8ec130c42
ChickenDinnerD <alexey333m@gmail.com>
2023-04-28 23:18:00 +0300

    old proj

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,18 @@ import bt2 from "./img/bt2.png"
 import btminus1 from "./img/btminus1.png"
 import btminus2 from "./img/btminus2.png"
 import btminus3 from "./img/btminus3.png"
+import bt02 from "./img/bt02.png"
+import bt03 from "./img/bt03.png"
+import bt04 from "./img/bt04.png"
+import bt05 from "./img/bt05.png"
+import bt06 from "./img/bt06.png"
+import bt07 from "./img/bt07.png"
+import bt08 from "./img/bt08.png"
+import bt09 from "./img/bt09.png"
+import bt10 from "./img/bt10.png"
+import bt11 from "./img/bt11.png"
+import bt12 from "./img/bt12.png"
+import bt13 from "./img/bt13.png"
 
 const imageMap = {
   btminus4: btminus4,
@@ -22,7 +34,19 @@ const imageMap = {
   bt2: bt2,
   btminus1: btminus1,
   btminus2: btminus2,
-  btminus3: btminus3
+  btminus3: btminus3,
+  bt02: bt02,
+  bt03: bt03,
+  bt04: bt04,
+  bt05: bt05,
+  bt06: bt06,
+  bt07: bt07,
+  bt08: bt08,
+  bt09: bt09,
+  bt10: bt10,
+  bt11: bt11,
+  bt12: bt12,
+  bt13: bt13,
 }
 
 export default function App() {
