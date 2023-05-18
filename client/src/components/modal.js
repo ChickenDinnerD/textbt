3bba99cb70d6c21332b21fb1af1a6c148a76bd1b
ChickenDinnerD <alexey333m@gmail.com>
2023-05-18 17:25:53 +0300

    final stage

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -20,7 +20,6 @@ function setModalMaxHeight() {
   }
 }
 
-
 document.addEventListener('DOMContentLoaded', function() {
   setModalMaxHeight();
 });
--- migrations/1679998372498_create_note_37.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "up": "INSERT INTO `notes_bt`.`notes` (`noteName`, `noteText`) VALUES ('мюсли', 'This is the first note.');",
-    "down": "DELETE FROM `notes_bt`.`notes` WHERE (`noteName` = `Note 1`);"
-  };
